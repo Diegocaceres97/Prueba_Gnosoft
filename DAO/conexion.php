661c2832f219a78a227b565c4b549d3b1cf915f5
Diegocaceres97 <diegocaceres1997garcia@gmail.com>
2021-04-04 15:08:39 -0500

    Edit/Change: cambio de servidor/db

--- DAO/conexion.php
+++ DAO/conexion.php
@@ -1,14 +1,14 @@
 <?php
 class Conexion{//Utilizamos PDO para hacer una conexión mas estable, fácil y con buen rendimiento
     public static function Conectar(){
-          define('servidor','localhost:3307'); /* Servidor de prueba local*/
+        /*  define('servidor','localhost:3307'); /* Servidor de prueba local/
           define('nombre_bd','gnosoft_prueba');
           define('usuario','root');
-          define('password','');
-        /* define('servidor','us-cdbr-east-02.cleardb.com');
+          define('password','');*/
+         define('servidor','us-cdbr-east-02.cleardb.com');
          define('nombre_bd','heroku_38246bdf586b553');
          define('usuario','b87f916543010e');
-         define('password','bb6d2e19');*/
+         define('password','bb6d2e19');
         $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
         try{
 $conexion = new PDO("mysql:host=".servidor."; dbname=".nombre_bd, usuario, password, $opciones);
